2ec8325f82c2f7356a70c1f13471a456b2feb2b6
btt-17 <btthanh.46@gmail.com>
2023-04-10 17:01:29 +0900

    add test_alter_table_multi_action()

--- keywords.py
+++ keywords.py
@@ -59,7 +59,7 @@ NUMERIC_TYPE = Token(r'(' + str(EXACT_NUMERIC_TYPE) + r'|' + str(APPROX_NUMERIC_
 
 CHARACTER_STRING_TYPE = Token(r'(CHAR|VARCHAR|CHARACTER)', TTYPE.type)
 
-PREDEFINED_TYPE = Token(r'('+str(NUMERIC_TYPE)+r"|"+str(CHARACTER_STRING_TYPE)+r")", TTYPE.type)
+PREDEFINED_TYPE = Token(str(NUMERIC_TYPE)+r"|"+str(CHARACTER_STRING_TYPE), TTYPE.type)
 
 DATA_TYPE = Token(r'('+str(PREDEFINED_TYPE)+r")", TTYPE.type)
 
@@ -75,11 +75,11 @@ COLUMN_DEFINITION = Token(  str(COLUMN_NAME)  + r'\s+' + r'(' + str(DATA_TYPE_OR
 
 ADD_COLUMN_DEFINITION = Token(r'(ADD\s+(?:\bCOLUMN\b)?)\s+' +r'(' + str(COLUMN_DEFINITION) + r')' , TTYPE.definition)
 
-ALTER_TABLE_ACTION = Token(r'(' + str(ADD_COLUMN_DEFINITION) + r')'  , TTYPE.action)
+ALTER_TABLE_ACTION = Token(r'(' + str(ADD_COLUMN_DEFINITION)  + r')'  , TTYPE.action)
 
 ALTER_TABLE_STATEMENT = Token(
     r'(ALTER TABLE)\s+' + r"(" 
-    + str(TABLE_NAME) +r")" + r"\s+" + r"(" +str(ALTER_TABLE_ACTION) + r")" + r".*", 
+    + str(TABLE_NAME) +r")" + r"\s+" + r"(?:" +str(ALTER_TABLE_ACTION)  + r"){1,}" + r".*", 
     TTYPE.statement.alter_table
 )
 
@@ -102,7 +102,6 @@ def test_alter_table_single_action():
     print("(TABLE_NAME): ", regex_match.group(2))
     print("(ALTER_TABLE_ACTION) group: ", regex_match.group(3))
   
-   
     alter_table_action_pattern = re.compile(str(ALTER_TABLE_ACTION), re.IGNORECASE)
     regex_match_2 = alter_table_action_pattern.match(regex_match.group(3))
  
@@ -118,9 +117,41 @@ def test_alter_table_single_action():
     print("(COLUMN NAME): ", regex_match_4.group(1))
     print("(DATA TYPE OR DOMAIN_NAME): ", regex_match_4.group(2))
 
+def test_alter_table_multi_action():
+    alter_table_statement_pattern = re.compile(str(ALTER_TABLE_STATEMENT), re.IGNORECASE)
+    sql_string = "ALTER TABLE test ADD COLUMN `count` SMALLINT  , ADD COLUMN `log` VARCHAR  , ADD COLUMN status INT "
+    regex_match = alter_table_statement_pattern.match(sql_string+" ,")
+    print("(ALTER TABLE) COMMAND: ", regex_match.group(1))
+    print("(TABLE_NAME): ", regex_match.group(2))
+
+    
+    actions_list = []
+    actions_list.append(regex_match.group(3))
+    actions_list.extend(sql_string.split(',')[1:])
+
+    for i, action in enumerate(actions_list):
+        print(f"===== Action {i+1} ====")
+        action = action.strip()
+        alter_table_action_pattern = re.compile(str(ALTER_TABLE_ACTION), re.IGNORECASE)
+        regex_match_2 = alter_table_action_pattern.match(action)
+    
+        print("(ADD COLUMN DEFINITION): ", regex_match_2.group(1))
+
+        add_column_definition_pattern = re.compile(str(ADD_COLUMN_DEFINITION), re.IGNORECASE)
+        regex_match_3 = add_column_definition_pattern.match(regex_match_2.group(1))
+        print("(ADD COLUMN) COMMAND: ", regex_match_3.group(1))
+        print("(COLUMN DEFINITION): ", regex_match_3.group(2))
+
+        column_definition_pattern = re.compile(str(COLUMN_DEFINITION), re.IGNORECASE)
+        regex_match_4 = column_definition_pattern.match(regex_match_3.group(2))
+        print("(COLUMN NAME): ", regex_match_4.group(1))
+        print("(DATA TYPE OR DOMAIN_NAME): ", regex_match_4.group(2))
+
+
 # Main function
 if __name__=="__main__":
-   test_alter_table_single_action()
+#    test_alter_table_single_action()
+   test_alter_table_multi_action()
 
 
     
